b70cbea6228a24974e149e03bb5c8dda4fe89ae4
Bogdan Gradinariu <bogdan.gradinariu@cynny.com>
2015-01-16 15:15:46 +0200

    first working copy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+//Handles HTML and wiring data
+//Using Three v60
+
+var events = new Events();
+
+var UberVizMain = function() {
+
+    function init() {
+        AudioHandler.init();
+        ControlsHandler.init();
+        update();
+    }
+
+    function update() {
+        requestAnimationFrame(update);
+        events.emit("update");
+    }
+
+    return {
+        init: init
+    };
+
+}();
+
+$(document).ready(function() {
+    UberVizMain.init();
+});
